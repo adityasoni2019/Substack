17aaaf2910b52f361212921836697ced0ee59de9
Aditya Soni <adityasoni9727@gmail.com>
2022-11-03 21:53:31 +0530

    Trying to add the sign in page

--- src/pages/Sign In Sign Up/Sign In/Sign-in.component.jsx
+++ src/pages/Sign In Sign Up/Sign In/Sign-in.component.jsx
@@ -2,6 +2,8 @@ import { computeHeadingLevel } from "@testing-library/react";
 import React from "react";
 import { Component } from "react";
 
+import {ReactComponent as Logo} from '../../../assets/logos/substack-brand-color.svg'
+
 // Now, we need this to be a class components, cause we need data. 
 
 class SignIn extends React.Component{
@@ -9,7 +11,13 @@ class SignIn extends React.Component{
     render(){
         return(
             <div className="signIn">
-                <p>HELLOS</p>
+
+                <div className="headser">
+
+                    <Logo className="logo"/>
+                    
+                </div>
+
             </div>
         );
     }
